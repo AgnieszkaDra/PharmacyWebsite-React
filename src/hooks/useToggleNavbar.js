f1e5f743d5160b54caa3d37da071c954ba1a6902
Agnieszka Draganczyk <Agnieszka.127@interia.pl>
2024-02-02 16:48:21 +0100

    fix bug in navigation

--- src/hooks/useToggleNavbar.js
+++ src/hooks/useToggleNavbar.js
@@ -1,9 +1,17 @@
 import { useState } from 'react'
+import { useMediaQuery } from '@react-hook/media-query'
 
 export const useToggleNavbar = () => {
-  const [navbarOpen, setNavbarOpen] = useState(false)
+  const [navbarOpen, setNavbarOpen] = useState(true)
+
+  const isMobile = useMediaQuery('(max-width: 768px)')
+
   const openNavbar = () => {
-    setNavbarOpen(!navbarOpen)
+    if (isMobile) {
+      setNavbarOpen(!navbarOpen)
+    } else {
+      //
+    }
   }
 
   return { navbarOpen, openNavbar }
